627553518d0dda3bccc87af682cefc7d55a1115c
Aleksey <Alecsey913@mail.ru>
2022-10-09 02:05:14 +0300

    Update Mutations

---
+++ Auto.Website/GraphQL/GraphTypes/InputOwnerGraphType.cs
@@ -0,0 +1,17 @@
+﻿using Auto.Data.Entities;
+using GraphQL.Types;
+
+namespace Auto.Website.GraphQL.GraphTypes;
+
+public class InputOwnerGraphType : InputObjectGraphType<Owner>
+{
+    public InputOwnerGraphType()
+    {
+        Name = "InputOwnerType";
+        Field<NonNullGraphType<StringGraphType>>("name");
+        Field<NonNullGraphType<StringGraphType>>("secondName");
+        Field<NonNullGraphType<StringGraphType>>("mail");
+        Field<NonNullGraphType<StringGraphType>>("phoneNumber");
+        Field<NonNullGraphType<StringGraphType>>("vehicleRegistration");
+    }
+}
--- Auto.Website/GraphQL/Mutations/OwnerMutation.cs
+++ Auto.Website/GraphQL/Mutations/OwnerMutation.cs
@@ -1,5 +1,7 @@
-﻿using Auto.Data;
+﻿using System;
+using Auto.Data;
 using Auto.Data.Entities;
+using Auto.Website.Controllers.Api;
 using Auto.Website.GraphQL.GraphTypes;
 using GraphQL;
 using GraphQL.Types;
@@ -15,40 +17,45 @@ public class OwnerMutation : ObjectGraphType
         _db = db;
 
         Field<OwnerGraphType>("createOwner")
-            .Argument<string>("name")
-            .Resolve(Create);
+            .Argument<InputOwnerGraphType>("owner")
+            .Resolve(CreateOwner);
         Field<OwnerGraphType>("updateOwner")
-            .Argument<int>("id")
-            .Argument<string>("newName")
-            .Argument<string>("newSecondName")
-            .Resolve(Update);
+            .Argument<NonNullGraphType<IntGraphType>>("id")
+            .Argument<InputOwnerGraphType>("owner")
+            .Resolve(UpdateOwner);
+        Field<OwnerGraphType>("deleteOwner")
+            .Argument<IntGraphType>("id")
+            .Resolve(DeleteOwner);
     }
 
-    private Owner Create(IResolveFieldContext<object> context)
+    private Owner CreateOwner(IResolveFieldContext<object> context)
     {
-        var name = context.GetArgument<string>("name");
-
-        var newOwner = new Owner
-        {
-            Name = name,
-            SecondName = "secondName",
-            Mail = "email",
-            PhoneNumber = "1111",
-            VehicleRegistration = "HVT266H"
-        };
-
-        var id = _db.CreateOwner(newOwner);
-
+        var owner = context.GetArgument<Owner>("owner");
+        var id = _db.CreateOwner(owner);
         return _db.FindOwner(id);
     }
 
-    private Owner Update(IResolveFieldContext<object> context)
+    
+    
+    private Owner UpdateOwner(IResolveFieldContext<object> context)
+    {
+        var updOwner = context.GetArgument<Owner>("owner");
+        var id = context.GetArgument<int>("id").ToString();
+        var owner = _db.FindOwner(id);
+        owner.Name = updOwner.Name;
+        owner.SecondName = updOwner.SecondName;
+        owner.Mail = updOwner.Mail;
+        owner.PhoneNumber = updOwner.PhoneNumber;
+        owner.VehicleRegistration = updOwner.VehicleRegistration;
+        _db.UpdateOwner(owner);
+        return _db.FindOwner(id);
+    }
+
+    private Owner DeleteOwner(IResolveFieldContext<object> context)
     {
         var id = context.GetArgument<int>("id").ToString();
         var owner = _db.FindOwner(id);
-        owner.Name = context.GetArgument<string>("newName");
-        owner.SecondName = context.GetArgument<string>("newSecondName");
-        _db.UpdateOwner(owner);
-        return _db.FindOwner(id);
+        _db.DeleteOwner(id);
+        return owner;
     }
 }
