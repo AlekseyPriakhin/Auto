df6845655c9e4afd8dfa170eb95da13b0f0f0d3c
Aleksey <Alecsey913@mail.ru>
2022-10-08 18:21:19 +0300

    Add queries

--- Auto.Website/GraphQL/Queries/OwnerQuery.cs
+++ Auto.Website/GraphQL/Queries/OwnerQuery.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.Intrinsics.Arm;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using Auto.Data;
 using Auto.Data.Entities;
 using Auto.Website.GraphQL.GraphTypes;
@@ -14,18 +17,50 @@ public class OwnerQuery : ObjectGraphType
     public OwnerQuery(IAutoDatabase db)
     {
         _db = db;
-        Field<OwnerGraphType>("owners")
-            .Argument<int>("ownerId")
-            .Resolve(GetOwner);
+        Field<OwnerGraphType>("ownerById")
+            .Argument<int>("id")
+            .Resolve(OwnerById);
+        Field<ListGraphType<OwnerGraphType>>("owners")
+            .Resolve(GetOwners);
+        Field<OwnerGraphType>("ownerByFullName")
+            .Argument<string>("firstName")
+            .Argument<string>("secondName")
+            .Resolve(OwnerByFullName);
+        Field<OwnerGraphType>("ownerByVehicleRegistration")
+            .Argument<string>("registration")
+            .Resolve(OwnerByVehicleRegistration);
+    }
+
+
+
+    private IEnumerable<Owner> GetOwners(IResolveFieldContext<object> context)
+    {
+        return _db.ListOwners();
     }
     
-
-
-    private Owner GetOwner(IResolveFieldContext<object> context)
+    private Owner OwnerById(IResolveFieldContext<object> context)
     {
-        var id = context.GetArgument<int>("ownerId").ToString();
-        var a = _db.FindOwner(id);
-        if (a == null) return null;
+        var id = context.GetArgument<int>("id").ToString();
         return _db.FindOwner(id);
     }
+
+    private Owner OwnerByVehicleRegistration(IResolveFieldContext<object> context)
+    {
+        var registration = context.GetArgument<string>("registration");
+        var owner = _db.FindVehicle(registration).Owner;
+        return owner;
+    }
+    
+    private Owner OwnerByFullName(IResolveFieldContext<object> context)
+    {
+        var firstName = context.GetArgument<string>("firstName");
+        var secondName = context.GetArgument<string>("secondName");
+        var owner = _db.ListOwners()
+            .FirstOrDefault(
+                o => o.Name.Equals(firstName) 
+                     && o.SecondName.Equals(secondName)
+                     );
+        return owner;
+    }
+    
 }
